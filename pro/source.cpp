2aaa3a18a7c05395829c64ca3e27d04fabea60a9
touristnct <64546545+touristnct@users.noreply.github.com>
2020-04-29 23:39:44 +0700

    Create source.cpp

---
+++ pro/source.cpp
@@ -0,0 +1,507 @@
+/**
+ *   Author   :   
+ *   Created  :  
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+
+string to_string(const string& s) {
+  return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+
+string to_string(vector<bool> v) {
+  bool first = true;
+  string res = "{";
+  for (int i = 0; i < static_cast<int>(v.size()); i++) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(v[i]);
+  }
+  res += "}";
+  return res;
+}
+
+template <size_t N>
+string to_string(bitset<N> v) {
+  string res = "";
+  for (size_t i = 0; i < N; i++) {
+    res += static_cast<char>('0' + v[i]);
+  }
+  return res;
+}
+
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+template <typename T>
+T inverse(T a, T m) {
+  T u = 0, v = 1;
+  while (a != 0) {
+    T t = m / a;
+    m -= t * a; swap(a, m);
+    u -= t * v; swap(u, v);
+  }
+  assert(m == 1);
+  return u;
+}
+
+template <typename T>
+class Modular {
+ public:
+  using Type = typename decay<decltype(T::value)>::type;
+
+  constexpr Modular() : value() {}
+  template <typename U>
+  Modular(const U& x) {
+    value = normalize(x);
+  }
+
+  template <typename U>
+  static Type normalize(const U& x) {
+    Type v;
+    if (-mod() <= x && x < mod()) v = static_cast<Type>(x);
+    else v = static_cast<Type>(x % mod());
+    if (v < 0) v += mod();
+    return v;
+  }
+
+  const Type& operator()() const { return value; }
+  template <typename U>
+  explicit operator U() const { return static_cast<U>(value); }
+  constexpr static Type mod() { return T::value; }
+
+  Modular& operator+=(const Modular& other) { if ((value += other.value) >= mod()) value -= mod(); return *this; }
+  Modular& operator-=(const Modular& other) { if ((value -= other.value) < 0) value += mod(); return *this; }
+  template <typename U> Modular& operator+=(const U& other) { return *this += Modular(other); }
+  template <typename U> Modular& operator-=(const U& other) { return *this -= Modular(other); }
+  Modular& operator++() { return *this += 1; }
+  Modular& operator--() { return *this -= 1; }
+  Modular operator++(int) { Modular result(*this); *this += 1; return result; }
+  Modular operator--(int) { Modular result(*this); *this -= 1; return result; }
+  Modular operator-() const { return Modular(-value); }
+
+  template <typename U = T>
+  typename enable_if<is_same<typename Modular<U>::Type, int>::value, Modular>::type& operator*=(const Modular& rhs) {
+#ifdef _WIN32
+    uint64_t x = static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value);
+    uint32_t xh = static_cast<uint32_t>(x >> 32), xl = static_cast<uint32_t>(x), d, m;
+    asm(
+      "divl %4; \n\t"
+      : "=a" (d), "=d" (m)
+      : "d" (xh), "a" (xl), "r" (mod())
+    );
+    value = m;
+#else
+    value = normalize(static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value));
+#endif
+    return *this;
+  }
+  template <typename U = T>
+  typename enable_if<is_same<typename Modular<U>::Type, int64_t>::value, Modular>::type& operator*=(const Modular& rhs) {
+    int64_t q = static_cast<int64_t>(static_cast<long double>(value) * rhs.value / mod());
+    value = normalize(value * rhs.value - q * mod());
+    return *this;
+  }
+  template <typename U = T>
+  typename enable_if<!is_integral<typename Modular<U>::Type>::value, Modular>::type& operator*=(const Modular& rhs) {
+    value = normalize(value * rhs.value);
+    return *this;
+  }
+
+  Modular& operator/=(const Modular& other) { return *this *= Modular(inverse(other.value, mod())); }
+
+  template <typename U>
+  friend const Modular<U>& abs(const Modular<U>& v) { return v; }
+
+  template <typename U>
+  friend bool operator==(const Modular<U>& lhs, const Modular<U>& rhs);
+
+  template <typename U>
+  friend bool operator<(const Modular<U>& lhs, const Modular<U>& rhs);
+
+  template <typename U>
+  friend std::istream& operator>>(std::istream& stream, Modular<U>& number);
+
+ private:
+  Type value;
+};
+
+template <typename T> bool operator==(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value == rhs.value; }
+template <typename T, typename U> bool operator==(const Modular<T>& lhs, U rhs) { return lhs == Modular<T>(rhs); }
+template <typename T, typename U> bool operator==(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) == rhs; }
+
+template <typename T> bool operator!=(const Modular<T>& lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(const Modular<T>& lhs, U rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(U lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
+
+template <typename T> bool operator<(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value < rhs.value; }
+
+template <typename T> Modular<T> operator+(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
+
+template <typename T> Modular<T> operator-(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
+
+template <typename T> Modular<T> operator*(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
+
+template <typename T> Modular<T> operator/(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
+
+template<typename T, typename U>
+Modular<T> power(const Modular<T>& a, const U& b) {
+  assert(b >= 0);
+  Modular<T> x = a, res = 1;
+  U p = b;
+  while (p > 0) {
+    if (p & 1) res *= x;
+    x *= x;
+    p >>= 1;
+  }
+  return res;
+}
+
+template <typename T>
+bool IsZero(const Modular<T>& number) {
+  return number() == 0;
+}
+
+template <typename T>
+string to_string(const Modular<T>& number) {
+  return to_string(number());
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& stream, const Modular<T>& number) {
+  return stream << number();
+}
+
+template <typename T>
+std::istream& operator>>(std::istream& stream, Modular<T>& number) {
+  typename common_type<typename Modular<T>::Type, int64_t>::type x;
+  stream >> x;
+  number.value = Modular<T>::normalize(x);
+  return stream;
+}
+
+/*
+using ModType = int;
+
+struct VarMod { static ModType value; };
+ModType VarMod::value;
+ModType& md = VarMod::value;
+using Mint = Modular<VarMod>;
+*/
+
+constexpr int md = (int) 1e9 + 7;
+using Mint = Modular<std::integral_constant<decay<decltype(md)>::type, md>>;
+
+template <typename T>
+class graph {
+ public:
+  struct edge {
+    int from;
+    int to;
+    T cost;
+  };
+
+  vector<edge> edges;
+  vector<vector<int>> g;
+  int n;
+
+  graph(int _n) : n(_n) {
+    g.resize(n);
+  }
+
+  virtual int add(int from, int to, T cost) = 0;
+};
+
+template <typename T>
+class forest : public graph<T> {
+ public:
+  using graph<T>::edges;
+  using graph<T>::g;
+  using graph<T>::n;
+
+  forest(int _n) : graph<T>(_n) {
+  }
+
+  int add(int from, int to, T cost = 1) {
+    assert(0 <= from && from < n && 0 <= to && to < n);
+    int id = (int) edges.size();
+    assert(id < n - 1);
+    g[from].push_back(id);
+    g[to].push_back(id);
+    edges.push_back({from, to, cost});
+    return id;
+  }
+};
+
+template <typename T>
+class dfs_forest : public forest<T> {
+ public:
+  using forest<T>::edges;
+  using forest<T>::g;
+  using forest<T>::n;
+
+  vector<int> pv;
+  vector<int> pe;
+  vector<int> order;
+  vector<int> pos;
+  vector<int> end;
+  vector<int> sz;
+  vector<int> root;
+  vector<int> depth;
+  vector<T> dist;
+
+  dfs_forest(int _n) : forest<T>(_n) {
+  }
+
+  void init() {
+    pv = vector<int>(n, -1);
+    pe = vector<int>(n, -1);
+    order.clear();
+    pos = vector<int>(n, -1);
+    end = vector<int>(n, -1);
+    sz = vector<int>(n, 0);
+    root = vector<int>(n, -1);
+    depth = vector<int>(n, -1);
+    dist = vector<T>(n);
+  }
+
+  void clear() {
+    pv.clear();
+    pe.clear();
+    order.clear();
+    pos.clear();
+    end.clear();
+    sz.clear();
+    root.clear();
+    depth.clear();
+    dist.clear();
+  }
+
+ private:
+  void do_dfs(int v) {
+    pos[v] = (int) order.size();
+    order.push_back(v);
+    sz[v] = 1;
+    for (int id : g[v]) {
+      if (id == pe[v]) {
+        continue;
+      }
+      auto &e = edges[id];
+      int to = e.from ^ e.to ^ v;
+      depth[to] = depth[v] + 1;
+      dist[to] = dist[v] + e.cost;
+      pv[to] = v;
+      pe[to] = id;
+      root[to] = (root[v] != -1 ? root[v] : to);
+      do_dfs(to);
+      sz[v] += sz[to];
+    }
+    end[v] = (int) order.size() - 1;
+  }
+
+  void do_dfs_from(int v) {
+    depth[v] = 0;
+    dist[v] = T{};
+    root[v] = v;
+    pv[v] = pe[v] = -1;
+    do_dfs(v);
+  }
+
+ public:
+  void dfs(int v, bool clear_order = true) {
+    if (pv.empty()) {
+      init();
+    } else {
+      if (clear_order) {
+        order.clear();
+      }
+    }
+    do_dfs_from(v);
+  }
+
+  void dfs_all() {
+    init();
+    for (int v = 0; v < n; v++) {
+      if (depth[v] == -1) {
+        do_dfs_from(v);
+      }
+    }
+    assert((int) order.size() == n);
+  }
+};
+
+template <typename T>
+class lca_forest : public dfs_forest<T> {
+ public:
+  using dfs_forest<T>::edges;
+  using dfs_forest<T>::g;
+  using dfs_forest<T>::n;
+  using dfs_forest<T>::pv;
+  using dfs_forest<T>::pos;
+  using dfs_forest<T>::end;
+  using dfs_forest<T>::depth;
+
+  int h;
+  vector<vector<int>> pr;
+
+  lca_forest(int _n) : dfs_forest<T>(_n) {
+  }
+
+  inline void build_lca() {
+    assert(!pv.empty());
+    int max_depth = 0;
+    for (int i = 0; i < n; i++) {
+      max_depth = max(max_depth, depth[i]);
+    }
+    h = 1;
+    while ((1 << h) <= max_depth) {
+      h++;
+    }
+    pr.resize(n);
+    for (int i = 0; i < n; i++) {
+      pr[i].resize(h);
+      pr[i][0] = pv[i];
+    }
+    for (int j = 1; j < h; j++) {
+      for (int i = 0; i < n; i++) {
+        pr[i][j] = (pr[i][j - 1] == -1 ? -1 : pr[pr[i][j - 1]][j - 1]);
+      }
+    }
+  }
+
+  inline bool anc(int x, int y) {
+    return (pos[x] <= pos[y] && end[y] <= end[x]);
+  }
+
+  inline int go_up(int x, int up) {
+    assert(!pr.empty());
+    up = min(up, (1 << h) - 1);
+    for (int j = h - 1; j >= 0; j--) {
+      if (up & (1 << j)) {
+        x = pr[x][j];
+        if (x == -1) {
+          break;
+        }
+      }
+    }
+    return x;
+  }
+
+  inline int lca(int x, int y) {
+    assert(!pr.empty());
+    if (anc(x, y)) {
+      return x;
+    }
+    if (anc(y, x)) {
+      return y;
+    }
+    for (int j = h - 1; j >= 0; j--) {
+      if (pr[x][j] != -1 && !anc(pr[x][j], y)) {
+        x = pr[x][j];
+      }
+    }
+    return pr[x][0];
+  }
+};
+
+template <typename T>
+class fenwick {
+ public:
+  vector<T> fenw;
+  int n;
+
+  fenwick(int _n) : n(_n) {
+    fenw.resize(n);
+  }
+
+  void modify(int x, T v) {
+    while (x < n) {
+      fenw[x] += v;
+      x |= (x + 1);
+    }
+  }
+
+  T get(int x) {
+    T v{};
+    while (x >= 0) {
+      v += fenw[x];
+      x = (x & (x + 1)) - 1;
+    }
+    return v;
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  return 0;
+}
