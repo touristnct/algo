75a4f0fc698f217aeea6534bfb594660c2cda889
touristnct <64546545+touristnct@users.noreply.github.com>
2020-04-29 23:38:14 +0700

    Create template.cpp

---
+++ pro/template.cpp
@@ -0,0 +1,53 @@
+/**
+ *   Auhor    :  
+ *   Created  :
+**/
+#include <bits/stdc++.h>
+
+#define fto(i, x, y) for (auto i = (x); i <= (y); i++)
+#define fdto(i, x, y) for (auto i = (x); i >= (y); i--)
+#define ftoa(i, x, y, a) for (auto i = (x); i <= (y); i += a)
+#define ftoit(it, var) for (auto it = var.begin(); it <= var.end(); it++)
+ 
+#define mp make_pair
+#define mt make_tuple
+
+#define fi first
+#define se second
+
+#define pb push_back
+#define eb emplace_back
+#define ef emplace_front
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+ 
+
+#define lb lower_bound
+#define ub upper_bound
+ 
+#define oo 1000000007LL
+#define OO 1000000000000000007LL
+
+using namespace std;
+ 
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpi;
+typedef vector<vi> vvi;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef pair<ll, ll> pll;
+typedef double ld;
+ 
+template<class T> bool uin(T &a, T b) { return a > b ? (a = b, true) : false; }
+template<class T> bool uax(T &a, T b) { return a < b ? (a = b, true) : false; }
+
+const int maxn = ;
+
+int main() {
+   ios_base::sync_with_stdio(false);
+   cin.tie(0);
+   return 0;
+}
