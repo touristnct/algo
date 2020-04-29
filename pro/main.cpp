1e34115b168db346c9ee29fa325836d999e8443e
touristnct <64546545+touristnct@users.noreply.github.com>
2020-04-29 23:38:59 +0700

    Create main.cpp

---
+++ pro/main.cpp
@@ -0,0 +1,13 @@
+/**
+ *   Author   :   
+ *   Created  :  
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  return 0;
+} 
